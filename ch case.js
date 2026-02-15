09e573d77e8267cca3e2aa5b8f1fb90436fcb095
Masayekh Ahammed <masayekh.ahammed@gmail.com>
2026-02-16 00:58:36 +0600

    case detector

---
+++ ch case.js
@@ -0,0 +1,9 @@
+
+let cha= prompt("Enter a character: ")
+if(cha=='a && b && c && d && f && g && h && j && k && l && m && n && p && q && r && s && t && v && w && x && y && z'){
+    console.log(cha+" is a small letter");
+}else if(cha=='A && B && C && D && F && G && H && J && K && L && M && N && P && Q && R && S && T && V && W && X && Y && Z'){
+    console.log(cha+" is a capital letter");
+}else if(cha=='0 && 1 && 2 && 3 && 4 && 5 && 6 && 7 && 8 && 9'){
+    console.log(cha+" is a number or digit");
+}
