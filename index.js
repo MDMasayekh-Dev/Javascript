0075db8bd21ffbe93803a003229eb4287559f921
Masayekh Ahammed <masayekh.ahammed@gmail.com>
2026-02-16 00:48:06 +0600

    change

---
+++ index.js
@@ -0,0 +1,6 @@
+let ch=prompt("Enter a character: ").toLowerCase();
+if(ch==a ||ch==e ||ch==i ||ch==o ||ch==u){
+    console.log("The character is a vowel.")
+}else{
+    console.log("The character is a consonant.")
+};
