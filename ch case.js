92660ddc2a03d119c551b8d92f7e7670382c87d6
Masayekh Ahammed <masayekh.ahammed@gmail.com>
2026-02-16 02:32:43 +0600

    character case detector

--- ch case.js
+++ ch case.js
@@ -1,7 +1,12 @@
 
-let cha= prompt("Enter a character: ")
-if(cha== 'a' && cha== 'b' && cha== 'c' && cha== 'd' && cha== 'e' && cha== 'f' && cha== 'g' && cha== 'h' && cha== 'i' && cha== 'j' && cha== 'k' && cha== 'l' && cha== 'm' && cha== 'n' && cha== 'o' && cha== 'p' && cha== 'q' && cha== 'r' && cha== 's' && cha== 't' && cha== 'u' && cha== 'v' && cha== 'w' && cha== 'x' && cha== 'y' && cha== 'z'){
-    console.log(cha+" is a small letter");
-}else(cha== 'A' && cha== 'B' && cha=='C' && cha=='D' && cha=='E' && cha=='F' && cha=='G' && cha=='H' && cha=='I' && cha=='J' && cha=='K' && cha=='L' && cha=='M' && cha=='N' && cha=='O' && cha=='P' && cha=='Q' && cha=='R' && cha=='S' && cha=='T' && cha=='U' && cha=='V' && cha=='W' && cha=='X' && cha=='Y' && cha=='Z'){
+let cha= prompt("Enter a character: ");
+if(cha>='A' && cha<='Z'){
     console.log(cha+" is a capital letter");
 }
+else if(cha>='a' && cha<='z'){
+    console.log(cha+" is a small letter");
+}
+
+
+    
+
