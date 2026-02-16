8bb84750f952dfb89bb7f0bf8962dae353536c48
Masayekh Ahammed <masayekh.ahammed@gmail.com>
2026-02-17 00:42:10 +0600

    new hw

---
+++ 3rd hw/ternary.js
@@ -0,0 +1,16 @@
+let age=  prompt("Enter your age");
+let jobsalary= prompt("Enter your salary");
+let OwnHouse= prompt("Do you have your own house? (yes/no)");
+
+
+age==30? 
+jobsalary>=40000? 
+OwnHouse=="yes"?
+
+console.log("Biye korte parbe"): log("Age bari banao"):
+
+
+console.log("Age valo job koro"):
+
+
+console.log("Biye korte parbe na");
