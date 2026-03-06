d8d7b274d9fe2ec3c9c316adb7fc94fcc08e5e7f
Masayekh Ahammed <masayekh.ahammed@gmail.com>
2026-03-07 01:16:12 +0600

    posting project

---
+++ project 3/js/script.js
@@ -0,0 +1,10 @@
+let name= document.querySelector(".name");
+let caption= document.querySelector(".caption");
+let post= document.querySelector(".post");
+let cardName= document.querySelector(".card-name");
+
+
+post.addEventListener("click", function(){
+    let nameValue= name.value;
+    let captionValue= caption.value;
+})
