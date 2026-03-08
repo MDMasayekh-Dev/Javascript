840ee75d768d8cf8f206f7fd003c05b9a123b810
Masayekh Ahammed <masayekh.ahammed@gmail.com>
2026-03-08 21:16:58 +0600

    project complete

--- project 3/js/script.js
+++ project 3/js/script.js
@@ -1,10 +1,48 @@
-let name= document.querySelector(".name");
-let caption= document.querySelector(".caption");
-let post= document.querySelector(".post");
-let cardName= document.querySelector(".card-name");
+let name = document.querySelector(".name");
+let caption = document.querySelector(".caption");
+let postBtn = document.querySelector(".post-btn");
+let postsContainer = document.querySelector(".posts");
+let error = document.querySelector(".error");
 
+let arr = [];
 
-post.addEventListener("click", function(){
-    let nameValue= name.value;
-    let captionValue= caption.value;
-})
+postBtn.addEventListener("click", () => {
+  if (!(name.value && caption.value)) {
+    name.style.borderColor = "red";
+    caption.style.borderColor = "red";
+
+    name.placeholder = "Please enter a name.";
+    caption.placeholder = "Please enter a caption.";
+  } else if (!isNaN(name.value)) {
+    error.innerHTML = "Please use letters only.";
+
+    name.style.borderColor = "red";
+    caption.style.borderColor = "red";
+
+    postBtn.disabled = true;
+  } else {
+    arr.push({
+      name: name.value,
+      caption: caption.value,
+    });
+
+    postsContainer.innerHTML = "";
+    display();
+
+    name.value = "";
+    caption.value = "";
+  }
+});
+
+function display() {
+  arr.map((item) => {
+    postsContainer.innerHTML += `<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h3 class="card-name">${item.name}</h3>
+    <p class="card-text">${item.caption}</p>
+    <button  class="btn btn-primary">Edit</button>
+    <button  class="btn btn-danger">Delete</button>
+  </div>
+</div>`;
+  });
+}
